cc14ac08ade7b44e8905adbfe504cadc6064390f
mroot <almatov.ms@gmail.com>
2021-01-13 15:59:33 +0500

    install tailwindcss; first ui configuration

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/cars', CarController::class);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
